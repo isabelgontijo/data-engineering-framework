1ce8641804b0c7091f93d1c1e10456b543053abc
Isabel Gontijo <66309653+isabelgontijo@users.noreply.github.com>
2025-05-25 18:49:23 -0300

    Create environments.py

---
+++ config/environments.py
@@ -0,0 +1,17 @@
+ENVIRONMENTS = {
+    'xxx': {
+        'env': 'dev',
+        'catalog': 'dev',
+        'storage': 'stodatalakedev'
+    },
+    'yyy': {
+        'env': 'stg',
+        'catalog': 'stg',
+        'storage': 'stodatalakeuat'
+    },
+    'zzz': {
+        'env': 'prd',
+        'catalog': 'prd',
+        'storage': 'stodatalakeprd'
+    }
+}
