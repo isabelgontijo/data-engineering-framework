366bd7856144286a6046fae7015d79960d2e3209
Isabel Gontijo <66309653+isabelgontijo@users.noreply.github.com>
2025-05-26 21:13:59 -0300

    Update get_notebook_context_info.py

--- src/get_notebook_context_info.py
+++ src/get_notebook_context_info.py
@@ -1,3 +1,5 @@
+import re
+
 def get_notebook_context_info():
     """
     Extract useful context information from Databricks notebook context,
@@ -9,31 +11,25 @@ def get_notebook_context_info():
     ctx = dbutils.notebook.entry_point.getDbutils().notebook().getContext()
 
     notebook_path = ctx.notebookPath().get()
-    # Example path: /Repos/dev/my-team/my-repo/...
-
     path_parts = notebook_path.strip("/").split("/")
 
-    # Validate expected structure
-    # Expected: Repos / <env_folder> / <team_folder> / <repo_folder> / ...
-    if len(path_parts) < 4 or path_parts[0].lower() != "repos":
-        raise ValueError(f"Unexpected notebook path structure: {notebook_path}")
+    browser_host_name = ctx.browserHostName().get()
+    workspace_id = re.search(r"adb-(\d+)", browser_host_name).group(1)
 
     info = {
         "notebook_path": notebook_path,
+        "env_folder": path_parts[1],
+        "team_folder": path_parts[2],
+        "repo_folder": path_parts[3]
         "api_url": ctx.apiUrl().get(),
         "api_token": ctx.apiToken().get(),
-        "browser_host_name": ctx.browserHostName().get(),
+        "browser_host_name": browser_host_name,
+        "workspace_id": workspace_id,
         "acl_path_of_acl_root": ctx.aclPathOfAclRoot().get(),
-        "mlflow_git_relative_path": ctx.mlflowGitRelativePath().get(),
         "notebook_id": ctx.notebookId().get(),
         "cluster_id": ctx.clusterId().get(),
         "user": ctx.user().get(),
         "full_context": ctx.safeAsJson(),
-
-        # Parsed path info:
-        "env_folder": path_parts[1],
-        "team_folder": path_parts[2],
-        "repo_folder": path_parts[3]
     }
 
     return info
