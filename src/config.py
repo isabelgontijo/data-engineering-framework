9d7407a6005d1ba00354bb50cb7bab9ce11de949
isabelgontijo <isabelgontijo>
2025-06-14 20:27:05 +0000

    Teste

--- src/config.py
+++ src/config.py
@@ -1,17 +1,17 @@
 ENVIRONMENTS = {
-    "dev": {
+    "1.dev": {
         "catalog": "dev",
-        "storage": "stodev",
-        "service_principal": "dev-service-principal-uuid"
+        # "storage": "stodev",
+        "service_principal": "e1cb5fd2-aaf9-40f5-b711-e9b4f4f486b8"
     },
-    "stg": {
+    "2.stg": {
         "catalog": "stg",
-        "storage": "stouat",
-        "service_principal": "stg-service-principal-uuid"
+        # "storage": "stouat",
+        "service_principal": "60f12ac4-0a80-49b7-8b19-9abd0dfddc68"
     },
-    "prd": {
+    "3.prd": {
         "catalog": "prd",
-        "storage": "stoprd",
-        "service_principal": "prd-service-principal-uuid"
+        # "storage": "stoprd",
+        "service_principal": "7e070d87-2cb6-4d1a-ba10-f2425a8dbfcb"
     }
 }
